6a016b9cbe86c2684a5b7740c5b822c8a3b8546b
Siddharth Vaish <sidvaish97@gmail.com>
2019-04-01 01:28:36 +0530

    suggestion working

--- app/src/main/java/com/siddharth/divinecare/MainActivity.java
+++ app/src/main/java/com/siddharth/divinecare/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -24,68 +26,127 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-   EditText etSearch;
-   Button btnSearch;
-   ListView lvVideo;
-   ArrayList<VideoDetails> videoDetailsArrayList;
-   CustomListAdapter customListAdapter;
-   String searchName;
-   String TAG="MainActivity";
-   String defURL="https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC0bB4q6DDEop428dHHraWVg&maxResults=24&order=date&type=video&key=AIzaSyCDgWb24K_JbWgJi8q9grBSm9N-bINUfPo";
+    AutoCompleteTextView etSearch;
+    Button btnSearch;
+    ListView lvVideo;
+    ArrayList<VideoDetails> videoDetailsArrayList;
+    CustomListAdapter customListAdapter;
+    String searchName;
+    String TAG = "MainActivity";
+    String defURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC0bB4q6DDEop428dHHraWVg&maxResults=24&order=date&type=video&key=AIzaSyCDgWb24K_JbWgJi8q9grBSm9N-bINUfPo";
+    private String[] SUGGESTIONS = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etSearch=(EditText)findViewById(R.id.et_search);
-        btnSearch=(Button)findViewById(R.id.btn_search);
-        lvVideo=(ListView)findViewById(R.id.videoList);
-        videoDetailsArrayList=new ArrayList<>();
-        customListAdapter=new CustomListAdapter(MainActivity.this,videoDetailsArrayList);
+        etSearch = findViewById(R.id.et_search);
+        findsuggestions();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, SUGGESTIONS);
+        etSearch.setAdapter(adapter);
+
+        btnSearch = (Button) findViewById(R.id.btn_search);
+        lvVideo = (ListView) findViewById(R.id.videoList);
+        videoDetailsArrayList = new ArrayList<>();
+        customListAdapter = new CustomListAdapter(MainActivity.this, videoDetailsArrayList);
         showVideo(defURL);
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                searchName=etSearch.getText().toString();
+                searchName = etSearch.getText().toString();
                 videoDetailsArrayList.clear();
-                showVideo("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC0bB4q6DDEop428dHHraWVg&maxResults=10&order=date&type=video&q="+searchName+"&key=AIzaSyCDgWb24K_JbWgJi8q9grBSm9N-bINUfPo");
+                showVideo("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC0bB4q6DDEop428dHHraWVg&maxResults=10&order=date&type=video&q=" + searchName + "&key=AIzaSyCDgWb24K_JbWgJi8q9grBSm9N-bINUfPo");
+                findsuggestions();
             }
         });
+
+    }
+    private void findsuggestions()
+    {
+        String URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC0bB4q6DDEop428dHHraWVg&maxResults=10&order=date&type=video&q=" + searchName + "&key=AIzaSyCDgWb24K_JbWgJi8q9grBSm9N-bINUfPo";
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray jsonArray = jsonObject.getJSONArray("items");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                        JSONObject jsonVideoId = jsonObject1.getJSONObject("id");
+                        //Log.e(TAG, "video ID" + jsonVideoId);
+                        JSONObject jsonsnippet = jsonObject1.getJSONObject("snippet");
+                        JSONObject jsonObjectdefault = jsonsnippet.getJSONObject("thumbnails").getJSONObject("medium");
+                        VideoDetails videoDetails = new VideoDetails();
+
+                        String videoid = jsonVideoId.getString("kind");
+                        //Log.e(TAG, " New Video Id" + videoid);
+                        videoDetails.setURL(jsonObjectdefault.getString("url"));
+                        videoDetails.setVideoName(jsonsnippet.getString("title"));
+                        videoDetails.setVideoDesc(jsonsnippet.getString("description"));
+                        String title=videoDetails.getVideoName();
+                        //Log.e(TAG,title);
+                        String[] words=title.split(" ");
+                        for(String eachWord : words) {
+                            int index=SUGGESTIONS.length-1;
+                            SUGGESTIONS[index]=eachWord;
+                            Log.e(TAG,eachWord);
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        int socketTimeout = 30000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        requestQueue.add(stringRequest);
 
     }
 
     private void showVideo(String url) {
 
-        String URL="https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC0bB4q6DDEop428dHHraWVg&maxResults=10&order=date&type=video&q="+searchName+"&key=AIzaSyCDgWb24K_JbWgJi8q9grBSm9N-bINUfPo";
-        RequestQueue requestQueue=Volley.newRequestQueue(getApplicationContext());
-        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        String URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC0bB4q6DDEop428dHHraWVg&maxResults=10&order=date&type=video&q=" + searchName + "&key=AIzaSyCDgWb24K_JbWgJi8q9grBSm9N-bINUfPo";
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
-                    JSONObject jsonObject=new JSONObject(response);
-                    JSONArray jsonArray=jsonObject.getJSONArray("items");
-                    for(int i=0;i<jsonArray.length();i++){
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray jsonArray = jsonObject.getJSONArray("items");
+                    for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject jsonObject1 = jsonArray.getJSONObject(i);
-                        JSONObject jsonVideoId=jsonObject1.getJSONObject("id");
-                        Log.e(TAG,"video ID"+jsonVideoId);
-                        JSONObject jsonsnippet= jsonObject1.getJSONObject("snippet");
+                        JSONObject jsonVideoId = jsonObject1.getJSONObject("id");
+                        //Log.e(TAG, "video ID" + jsonVideoId);
+                        JSONObject jsonsnippet = jsonObject1.getJSONObject("snippet");
                         JSONObject jsonObjectdefault = jsonsnippet.getJSONObject("thumbnails").getJSONObject("medium");
-                        VideoDetails videoDetails=new VideoDetails();
+                        VideoDetails videoDetails = new VideoDetails();
 
-                        String videoid=jsonVideoId.getString("kind");
-                        Log.e(TAG," New Video Id" +videoid);
+                        String videoid = jsonVideoId.getString("kind");
+                        //Log.e(TAG, " New Video Id" + videoid);
                         videoDetails.setURL(jsonObjectdefault.getString("url"));
                         videoDetails.setVideoName(jsonsnippet.getString("title"));
                         videoDetails.setVideoDesc(jsonsnippet.getString("description"));
-                     //   videoDetails.setVideoId(videoID);
+                        //   videoDetails.setVideoId(videoID);
 
-                       videoDetailsArrayList.add(videoDetails);
+                        videoDetailsArrayList.add(videoDetails);
 
 //                         videoDetailsArrayList.clear();
                     }
                     lvVideo.setAdapter(customListAdapter);
 
-             customListAdapter.notifyDataSetChanged();
-                   // lvVideo.setAdapter(null);
+                    customListAdapter.notifyDataSetChanged();
+                    // lvVideo.setAdapter(null);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
